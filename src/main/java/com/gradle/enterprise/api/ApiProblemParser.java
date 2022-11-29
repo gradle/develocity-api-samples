48e85c10484ce9ea0de0b2bcfc437722f3c600ef
Iurii Ignatko <iignatko@gradle.com>
2022-11-29 18:14:18 +0200

    Adjust codestyle and throw UnchekedIOException instead of Runtime one (#38)

--- src/main/java/com/gradle/enterprise/api/ApiProblemParser.java
+++ src/main/java/com/gradle/enterprise/api/ApiProblemParser.java
@@ -5,21 +5,23 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.gradle.enterprise.api.client.ApiException;
 import com.gradle.enterprise.api.model.ApiProblem;
 
+import java.io.UncheckedIOException;
 import java.util.Optional;
 
 public final class ApiProblemParser {
-    private final static ObjectMapper OBJECT_MAPPER = new ObjectMapper();
+
+    private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
     private static final String CONTENT_TYPE = "application/problem+json";
 
     public static Optional<ApiProblem> maybeParse(ApiException apiException) {
         return apiException.getResponseHeaders()
             .firstValue("content-type")
-            .filter(v -> v.startsWith(CONTENT_TYPE))
+            .filter(contentType -> contentType.startsWith(CONTENT_TYPE))
             .map(__ -> {
                 try {
                     return OBJECT_MAPPER.readValue(apiException.getResponseBody(), ApiProblem.class);
                 } catch (JsonProcessingException e) {
-                    throw new RuntimeException(e);
+                    throw new UncheckedIOException(e);
                 }
             });
     }
